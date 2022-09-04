9031712d38ed5de8e3c18af61942b430f11d6595
Ron Gilmour <rgilmour70@gmail.com>
2022-09-04 17:40:35 -0400

    trying for real

--- src/api.js
+++ src/api.js
@@ -12,11 +12,37 @@ app.use(cors());
 
 router.get('/', async (req, res) => {
     const privateKey = process.env.authkey;
-    res.json({
-        'fruit': 'banana',
-        'privateKey': privateKey,
-        'vegetable': 'jicama'
-    })
+    const token = jwt.sign(
+        {
+          sub: 'com.gilmour.weather',
+        },
+        privateKey,
+        {
+          jwtid: 'QA25HR6SCP.com.gilmour.weather',
+          issuer: 'QA25HR6SCP',
+          expiresIn: '1h',
+          keyid: 'KT557LU647',
+          algorithm: 'ES256',
+          header: {
+            id: 'QA25HR6SCP.com.gilmour.weather',
+          }
+        }
+      );
+
+    const url = 'https://weatherkit.apple.com/api/v1/weather/en-us/42.41658/-76.48944?dataSets=currentWeather';
+      
+    const config = {
+        headers: { Authorization: `Bearer ${token}` }
+    };
+    
+    const { data: weatherData } = await axios.get(url, config);
+
+    res.json(weatherData);
+    // res.json({
+    //     'fruit': 'banana',
+    //     'privateKey': privateKey,
+    //     'vegetable': 'jicama'
+    // })
 });
 
 router.get('/mango', (req, res) => {
