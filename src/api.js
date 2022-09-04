916c8de12890ae4fd86f02d766dd70ec90645979
Ron Gilmour <rgilmour70@gmail.com>
2022-09-04 18:28:03 -0400

    try to parse for JSON

--- src/api.js
+++ src/api.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(cors());
 
 router.get('/', async (req, res) => {
-    const privateKey = process.env.authkey;
+    const privateKey = JSON.parse(process.env.authkey);
     const token = jwt.sign(
         {
           sub: 'com.gilmour.weather',
