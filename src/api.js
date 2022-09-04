9df8bda270933732fc6d4e397ef4e6c21cf4932d
Ron Gilmour <rgilmour70@gmail.com>
2022-09-04 18:53:17 -0400

    giving up soon

--- src/api.js
+++ src/api.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(cors());
 
 router.get('/', async (req, res) => {
-    const privateKey = fs.readFileSync("AuthKey_KT557LU647.p8") || process.env.authkey;
+    const privateKey = process.env.authkey;
     const token = jwt.sign(
         {
           sub: 'com.gilmour.weather',
