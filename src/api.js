afda3b65c7e516e251df624432550a9863dade06
Ron Gilmour <rgilmour70@gmail.com>
2022-09-05 08:54:00 -0400

    back to the regex

--- src/api.js
+++ src/api.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(cors());
 
 router.get('/', async (req, res) => {
-    const privateKey = process.env.authkey;
+    const privateKey = process.env.authkey.replaceAll(/_/, "\n");
     const token = jwt.sign(
         {
           sub: 'com.gilmour.weather',
