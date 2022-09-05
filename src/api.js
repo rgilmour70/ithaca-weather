8c62240222a0d2b154925d2c1bb1bcd7118fafc9
Ron Gilmour <rgilmour70@gmail.com>
2022-09-05 09:03:23 -0400

    reversed terms in or statement

--- src/api.js
+++ src/api.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(cors());
 
 router.get('/', async (req, res) => {
-    const privateKey = fs.readFileSync("AuthKey_KT557LU647.p8") || process.env.authkey.replaceAll(/_/g, "\n");
+    const privateKey = process.env.authkey.replaceAll(/_/g, "\n") || fs.readFileSync("AuthKey_KT557LU647.p8");
     const token = jwt.sign(
         {
           sub: 'com.gilmour.weather',
