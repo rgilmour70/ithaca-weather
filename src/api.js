f7e4b87b83ae25026623a35afe94e4954573bac8
Ron Gilmour <rgilmour70@gmail.com>
2022-09-08 14:40:41 -0400

    corrections to paths

--- src/api.js
+++ src/api.js
@@ -9,8 +9,8 @@ const path = require('path');
 
 const app = express();
 
-// app.use(express.static(path.join(__dirname, '/public')));
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, '/views')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.set('view engine', 'ejs');
 app.engine('ejs', require('ejs').__express);
