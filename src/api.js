1df6e28b2d84aa60f631ddd97f68e71e5a435a7e
Ron Gilmour <rgilmour70@gmail.com>
2022-09-09 07:45:01 -0400

    tweak to paths

--- src/api.js
+++ src/api.js
@@ -9,7 +9,7 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, '/views')));
+app.use(express.static(path.join(__dirname, 'views')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.set('view engine', 'ejs');
